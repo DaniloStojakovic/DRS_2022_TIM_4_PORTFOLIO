5b13069430af7aeb945a06753ff0ad9a10a71ee2
DaniloStojakovic <fqwdqdwqd@gmail.com>
2022-12-27 16:30:03 +0100

    Klase

---
+++ classes/Kriptovaluta.py
@@ -0,0 +1,7 @@
+class Kriptovaluta:
+    def init(self, naziv, kupovna_cena, kolicina, datum, vlasnik):
+            self.naziv = naziv
+            self.kupovna_cena = kupovna_cena
+            self.kolicina = kolicina
+            self.datum = datum
+            self.vlasnik = vlasnik
