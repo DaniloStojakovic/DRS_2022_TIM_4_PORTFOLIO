47163533d1bb41b1dd000f55872320c55eafece9
Kumkvat13 <a.momcilov13@gmail.com>
2022-12-28 19:12:01 +0100

    Main Page
    Front styling of Login and Registration

--- client/src/App.js
+++ client/src/App.js
@@ -14,12 +14,65 @@ function App() {
       }
     }).then(data => setState(data))
   }, [])
-
+/*
   return (
     <div className="App">
       <Channel data={initialState}/>
+    </div> 
+  )
+
+*/ 
+  const [usernameReg, setUsernameReg] = useState("");
+  const [passwordReg, setPasswrodReg] = useState("");
+
+  return(
+    <div className="App">
+      <div className="registration">
+        <h1>Registration</h1>
+      <label>Username:</label>
+      <input 
+        type="text" 
+        onChange={(e) => {
+          setUsernameReg(e.target.value);
+        }}/>
+      <br></br>
+      <label>Surname: </label>
+      <input type="text" />
+      <br></br>
+      <label>Address: </label>
+      <input type="text" />
+      <br></br>
+      <label>State: </label>
+      <input type="text" />
+      <br></br>
+      <label>Telephone number: </label>
+      <input type="text" />
+      <br></br>
+      <label>Email: </label>
+      <input type="text" />
+      <br></br>
+      <label>Password: </label>
+      <input 
+        type="text" 
+        onChange={(e) => {
+          setPasswrodReg(e.target.value);
+        }}/>
+      <br></br>
+      <button>Register</button>
+      </div>
+      <div className="login">
+        <h1>Login</h1>
+        <input type="text" placeholder="Username" />
+        <br></br>
+        <input type="text" placeholder="Password" />
+        <br></br>
+        <button>Login</button>
+      </div>
+      
     </div>
-  );
-}
+  )
+  
+
+  }
 
 export default App;
--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import User
 
 app = Flask(__name__)
 
