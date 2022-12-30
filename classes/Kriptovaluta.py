ac8e85b0902eb67e8a2ad441f4185ccb3b693443
DaniloStojakovic <fqwdqdwqd@gmail.com>
2022-12-30 19:02:05 +0100

    Baza
    baza podataka gotova konekcija - radi

--- classes/Kriptovaluta.py
+++
@@ -1,7 +0,0 @@
-class Kriptovaluta:
-    def init(self, naziv, kupovna_cena, kolicina, datum, vlasnik):
-            self.naziv = naziv
-            self.kupovna_cena = kupovna_cena
-            self.kolicina = kolicina
-            self.datum = datum
-            self.vlasnik = vlasnik
--- server/app.py
+++ server/app.py
@@ -1,37 +1,14 @@
-from flask import Flask, jsonify
-import pymongo
-from pymongo import MongoClient
+from flask import Flask
+import User
 
 app = Flask(__name__)
-"""
-def get_db():
-    client = MongoClient(host='test_mongodb',
-                         port=27017, 
-                         username='root', 
-                         password='pass',
-                        authSource="admin")
-    db = client["animal_db"]
-    return db
 
-@app.route('/')
-def ping_server():
-    return "Welcome to baza"
-
-@app.route('/animals')
-def get_stored_animals():
-    db = get_db()
-    _animals = db.animal_tb.find()
-    animals = [{"id": animal["id"], "name": animal["name"], "type": animal["type"]} for animal in _animals]
-    return jsonify({"animals": animals})
-
-"""
 @app.route('/api', methods=['GET'])
 def index():
     return {
-        "channel": "front",
-        "tutorial": "front"
+        "channel": "The Show",
+        "tutorial": "React, Flask and Docker"
     }
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
